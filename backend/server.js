bc2a9fbdf9c44de451c13b8265f80c195ef9ade5
Francisco Sabino <fdasabino@gmail.com>
2022-09-14 00:28:49 +0200

    added custom error handler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
-
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 dotenv.config();
 connectDB();
 
@@ -12,16 +12,13 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(errorHandler);
+app.use(notFound);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(
   PORT,
   console.log(`SERVER RUNNING IN ${process.env.NODE_ENVIRONMENT} MODE ON PORT ${PORT}`)
