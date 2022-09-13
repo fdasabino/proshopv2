e22dc1d38c841540997145b59b01fa00b1645cd1
Francisco Sabino <fdasabino@gmail.com>
2022-09-13 22:00:07 +0200

    added basic backend server functionality

--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-
 import Rating from "../Rating/Rating";
 import { Link } from "react-router-dom";
-
 import { Card } from "react-bootstrap";
 
 const Product = ({ product }) => {
--- frontend/src/screens/Home/Home.jsx
+++ frontend/src/screens/Home/Home.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 import Product from "../../components/Product/Product";
 
 import { Row, Col } from "react-bootstrap";
 
-import products from "../../products";
-
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
