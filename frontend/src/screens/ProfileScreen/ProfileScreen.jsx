26caa960d6175a47da793daddcd6ed5e61785246
Francisco Sabino <fdasabino@gmail.com>
2022-09-17 16:47:32 +0200

    added accordion in profile screen

--- frontend/src/screens/ProfileScreen/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen/ProfileScreen.jsx
@@ -3,13 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { USER_CONSTANT_TYPES } from "../../redux-store/constants/userConstants";
 import { getUserDetails, updateUserProfile } from "../../redux-store/actions/userActions";
-import { Container, Form, Button, Row, Col, Alert } from "react-bootstrap";
+import { Container, Form, Button, Row, Col, Alert, Accordion } from "react-bootstrap";
 import Spinner from "../../components/Spinner/Spinner";
 import toast from "react-hot-toast";
 
 const ProfileScreen = () => {
   const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState(null);
@@ -31,7 +30,6 @@ const ProfileScreen = () => {
       dispatch(getUserDetails("profile"));
     } else {
       setName(user.name);
-      setEmail(user.email);
     }
   }, [dispatch, navigate, userInfo, user]);
 
@@ -41,7 +39,7 @@ const ProfileScreen = () => {
       setMessage("Passwords do not match. Please try again");
     } else {
       //dispatch update profile
-      dispatch(updateUserProfile({ id: user._id, name, email, password }));
+      dispatch(updateUserProfile({ id: user._id, name, password }));
       toast.success("User profile updated...");
     }
   };
@@ -54,58 +52,55 @@ const ProfileScreen = () => {
       {loading && <Spinner />}
       <Row className="d-flex align-items-between">
         <Col md={9}>
-          <h3>Orders</h3>
+          <h4>Orders</h4>
           <hr />
         </Col>
 
         <Col md={3}>
-          <h3>Update Profile</h3>
-          <hr />
-
-          <Form
-            onSubmit={submitHandler}
-            className="d-flex align-items-center flex-column gap-3 text-center"
-          >
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control
-                type="name"
-                placeholder="Enter Full Name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Label>Email Address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter Email Address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Update Profile
-            </Button>
-          </Form>
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>
+                <hr /> <h5>Update Profile</h5>
+              </Accordion.Header>
+              <Accordion.Body>
+                <Form
+                  onSubmit={submitHandler}
+                  className="d-flex align-items-center flex-column gap-3 text-center"
+                >
+                  <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control
+                      type="name"
+                      placeholder="Enter Full Name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Confirm Password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Update Profile
+                  </Button>
+                </Form>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </Col>
       </Row>
     </Container>
